9cfbcfee9000203ef4a229087f6718000d9881c2
THAKAORI <kyutaro.1.19@gmail.com>
2021-06-20 18:28:07 +0000

    add code

--- s2-3/p67.py
+++ s2-3/p67.py
@@ -9,14 +9,25 @@ class Tyouhuku():
         self.dp = np.zeros((self.n+1, self.m+1), dtype=np.int32)
 
     def solve(self):
-        self.dp[0, 0] = 1
+        for i in range(self.n+1):
+            self.dp[i, 0] = 1
         for i in range(1, self.n+1):
             for j in range(1,self.m+1):
-                if(j > self.a[i-1]):
-                    self.dp[i, j] = self.dp[i-1, j] + self.dp[i, j-1] - self.dp[i-1, j-1-self.a[i]]
+                if(j - 1 - self.a[i-1] >= 0):
+                    self.dp[i, j] = self.dp[i-1, j] + self.dp[i, j-1] - self.dp[i-1, j-1-self.a[i-1]]
                 else:
                     self.dp[i, j] = self.dp[i-1, j] + self.dp[i, j-1]
         
+        print(self.dp)
+    def solve2(self):
+        for i in range(self.n+1):
+            self.dp[i, 0] = 1
+        for i in range(self.n):
+            for j in range(1, self.m+1):
+                if(j - 1 - self.a[i] >= 0):
+                    self.dp[i+1, j] = self.dp[i+1, j-1] + self.dp[i, j] - self.dp[i, j-1-self.a[i]]
+                else:
+                    self.dp[i+1, j] = self.dp[i+1, j-1] + self.dp[i, j]
         print(self.dp)
 
 if __name__ == '__main__':
